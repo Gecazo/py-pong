97a7d47e7e49526d2c820fe16583032c9640a22e
Filip <filip.zalozyk@gmail.com>
2022-05-26 17:33:18 +0200

    Paddles created

--- main.py
+++ main.py
@@ -4,39 +4,46 @@ pygame.init()
 WHITE, BLACK = (255, 255, 255), (0, 0, 0)
 WIDTH, HEIGHT = 400, 400
 
+PADDLE_WIDTH, PADDLE_HEIGHT = 20, 100
+
 FPS = 120
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('pong')
 
 
-class Paddle:
+class Paddle():
     COLOR = WHITE
 
-    def __init__(self, x, y, WIDTH, HEIGHT, color):
+    def __init__(self, x, y, width, height, color):
         self.x = x
         self.y = y
-        self.WIDTH = WIDTH
-        self.HEIGHT = HEIGHT
+        self.width = width
+        self.height = height
 
-        def draw(self, screen):
-            pygame.draw.rectangle(screen, self.COLOR,
-                                  (self.x, self.y, self.WIDTH, self.HEIGHT))
+    def draw(self, screen):
+        pygame.draw.rect(screen, self.COLOR, (self.x, self.y, self.width, self.height))
 
 
-def draw(screen):
+def draw(screen, paddles):
     screen.fill(BLACK)
+
+    for paddle in paddles:
+        paddle.draw(screen)
+
     pygame.display.update()
 
 
 def main():
     clock = pygame.time.Clock()
-    clock.tick(FPS)
 
-    draw(screen)
+    left_paddle = Paddle(10, HEIGHT//2 - PADDLE_HEIGHT//2, PADDLE_WIDTH, PADDLE_HEIGHT, WHITE)
+    right_paddle = Paddle(WIDTH - 10 - PADDLE_WIDTH, HEIGHT//2 - PADDLE_HEIGHT//2, PADDLE_WIDTH, PADDLE_HEIGHT, WHITE)
 
     running = True
     while running:
+        clock.tick(FPS)
+        draw(screen, [left_paddle, right_paddle])
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
